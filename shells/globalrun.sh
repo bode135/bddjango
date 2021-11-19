0831efcb88bcfb47751599ddf11699da7abb01f3
bode135 <2248270222@qq.com>
2021-11-19 17:32:56 +0800

    delete_shells

--- shells/globalrun.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-python online_run.py runserver 0.0.0.0:8080
--- shells/migrate_db.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-: Django数据库操作
-
-commit_name="$1"
-python manage.py makemigrations commit_name
-python manage.py migrate commit_name
