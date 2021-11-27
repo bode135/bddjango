ad1623cfcfe52511a1462c205861dcb5c46d5374
bode135 <2248270222@qq.com>
2021-11-28 01:13:57 +0800

    force_pull__bransh

--- shells/force_pull.sh
+++ shells/force_pull.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # 强制pull, 将本地重置为server端版本.
 
+branch=master
+
 git fetch --all
-git reset --hard origin/master
-git pull origin master
+git reset --hard origin/$branch
+git pull origin $branch
