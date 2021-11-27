496e9bb16bdd6218802b23cb37c8c5104bba991e
bode135 <2248270222@qq.com>
2021-11-28 01:12:20 +0800

    1

---
+++ auto.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+: 杀死指定端口, linux和mac版本
+
+# 加载用户名和密码
+source authinfo.sh
+
+
+echo $username
+echo $passwd
+
+# 删除旧文件, 编译新文件, expect自动上传pypi
+rm -rf build dist
+python setup.py sdist bdist_wheel
+expect myexpectupload.sh $username $passwd
+
