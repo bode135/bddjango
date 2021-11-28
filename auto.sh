1c2541f536c30940ab93dca4384390bf56e9c39c
bode135 <2248270222@qq.com>
2021-11-28 14:34:34 +0800

    .md转.rst

--- auto.sh
+++ auto.sh
@@ -12,7 +12,7 @@ echo $passwd
 git pull
 rm -rf build dist
 python setup.py sdist bdist_wheel
-echo "\n\n--- expect: end auto build.\n" && sleep 1
+echo "\n\n--- expect: end auto build.\n\n" && sleep 1
 
 expect myexpectupload.sh $username $passwd
-echo "\n\n--- expect: end auto submit.\n" && sleep 1
+echo "\n\n--- expect: end auto submit.\n\n" && sleep 1
