0bf11cfd42c535369efcc1b1a8bfec199d10b6a2
bode135 <2248270222@qq.com>
2021-11-28 14:39:51 +0800

    格式化输入输出

--- auto.sh
+++ auto.sh
@@ -5,14 +5,15 @@
 source authinfo.sh
 
 
-echo $username
-echo $passwd
+echo "--- your username: $username"
+echo "--- passwd: $passwd"
+sleep 1.5
 
 # git更新, 删除旧文件, 编译新文件, expect自动上传pypi
 git pull
 rm -rf build dist
 python setup.py sdist bdist_wheel
-echo -e "\n\n--- expect: end auto build.\n\n" && sleep 1
+echo -e "\n\n--- expect: end auto build.\n\n" && sleep 2
 
 expect myexpectupload.sh $username $passwd
-echo -e "\n\n--- expect: end auto submit.\n\n" && sleep 1
+echo -e "\n\n--- expect: end auto submit.\n\n" && sleep 2
